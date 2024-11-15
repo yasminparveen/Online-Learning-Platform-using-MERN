d1117a73ea3e44a6af67dc8ed5d2faad503d845d
NIVEDHA P M <121153649+nivedhapm@users.noreply.github.com>
2024-11-15 22:12:23 +0530

    Add files via upload

---
+++ Backend/schemas/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+      set: function (value) {
+        return value.charAt(0).toUpperCase() + value.slice(1);
+      },
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    type: {
+      type: String,
+      required: [true, "type is required"],
+    },
+    // enrolledCourses: [
+    //   {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "course",
+    //   },
+    // ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userSchema = mongoose.model("user", userModel);
+
+module.exports = userSchema;
